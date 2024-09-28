92b2dd8443de94d8668f46bd205a5e74c04930b0
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-09-28 20:57:20 +0530

    Create addition.c

---
+++ Tutorials/addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+
+    printf("Enter the value of a:- ");
+    scanf("%d", &a);
+    printf("Enter the value of b:- ");
+    scanf("%d", &b);
+
+    int add = a+b;
+    printf ("The addition of the two numbers is:- %d", add);
+
+    return 0;
+}
